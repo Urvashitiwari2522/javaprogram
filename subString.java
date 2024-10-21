a1570983b7e7626984c6876611ba899479ddd33d
Urvashi tiwari <priyatiwari0425@gmail.com>
2024-10-21 15:09:22 +0530

    Add files via upload

---
+++ subString.java
@@ -0,0 +1,12 @@
+            //substring 
+
+class Strings
+{
+ public static void main(String []X)
+ {
+	 String sentence = "My Name is Urvashi";
+     //substring = (beg index,end index)	
+	 String name = sentence.substring(11,sentence.length());
+     System.out.println(name);	 
+ } 
+}
