37a4a50c8fcaa71dce153fb651f02f28d03ae1c2
Urvashi tiwari <priyatiwari0425@gmail.com>
2024-12-18 22:08:23 +0530

    Add files via upload

---
+++ NumberPatternPrinting.java
@@ -0,0 +1,14 @@
+public class NumberPatternPrinting {
+public static void main(String[]args){
+    //Outer loop for row
+    for(int i=1;i<=5;i++){
+        //Inner loop for column
+        for(int j=1;j<=i;j++){
+        System.out.print(i);
+    //Print the current row number
+    }
+      System.out.println();
+      //Move to the next line after each row
+    }
+}    
+}
