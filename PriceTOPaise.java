261e3bddba96d27527059a8452f5501ffb297306
Urvashi tiwari <priyatiwari0425@gmail.com>
2025-04-19 21:56:05 +0530

    Update PriceTOPaise.java

--- PriceTOPaise.java
+++ PriceTOPaise.java
@@ -5,7 +5,7 @@ class PriceToPaise{
 		//create an object scanner for  user input
         Scanner scanner = new Scanner(System.in);
 		//Input from user
-    System.out.println("Enter the price of an item in decimal form (eg: 76.45):");
+    System.out.println(" Enter the price of an item in decimal form (eg: 76.45):");
     double price = scanner.nextDouble();
     //Conver the price to paise (1 Rupee = 100 Paise)
     int paise = (int)Math.round(price*100);
